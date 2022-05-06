ca75e03fd25fc63f492e9a6216dcdbd07c9e95ea
Miguel Martins <miguelalexandre.jesusdasilva@usc.es>
2022-05-06 17:25:35 +0200

    Modified files to read Auger Inclined data

--- Tau_Distribution/Compute_Estimator/Compute_Estimator.py
+++ Tau_Distribution/Compute_Estimator/Compute_Estimator.py
@@ -10,6 +10,9 @@ from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 #for data manipulation
 import pandas as pd
 
+#for manipulation and conversion between times
+from astropy.time import Time
+
 #to read files
 import os
 
@@ -119,96 +122,129 @@ def ComulativeDistFunc(data, nbins):
 
     return pdf_bin_edges[1:], cdf
 
+#draws an exponential envelop
+def LogExpEnvelop(x, rate):
+
+    return rate*np.exp(-rate*np.exp(x) + x)
+
 #set path to dir with uniform dist files
-path_to_dir_ud = '/home/miguelm/Documents/Anisotropies/Repeater_Analysis/DataSets/UD_large_stats/'
-path_to_dir_rep = '/home/miguelm/Documents/Anisotropies/Repeater_Analysis/DataSets/MockData_Repeaters/Rep_large_stats/'
+path_to_dir_ud = '/home/miguel/Documents/Repeaters_Analysis/DataSets/Inclined/UD_AugerOpenData_stats'
+#path_to_dir_rep = '/home/miguelm/Documents/Anisotropies/Repeater_Analysis/DataSets/MockData_Repeaters/Rep_large_stats/'
 
 #define tau threshold in log10(tau) with tau in days
 tau_th = 0
 
 #list to hold all tau values from all data sets of isotropy
-tau_ud_all, list_of_ordered_taus_ud = FromFiles_to_TauDist(path_to_dir_ud, 'Ud_events_with_tau')
+tau_ud_all, list_of_ordered_taus_ud = FromFiles_to_TauDist(path_to_dir_ud, 'UD_InclinedEvents_with_tau_3326')
+
 #tau_rep_all, list_of_ordered_taus_rep = FromFiles_to_TauDist(path_to_dir_rep, 'Rep_events_with_tau')
 
 list_of_log_tau_arrays = [np.log10(tau) for tau in list_of_ordered_taus_ud]
 
 #read file with tau values for repeater data
-path_to_repeaters = '/home/miguelm/Documents/Anisotropies/Repeater_Analysis/DataSets/MockData_Repeaters/'
-PERIOD_OF_REP = '3600'
-N_ACCEPTED_REP_EVENTS = '100'
-N_INTENSITY = '5'
+# path_to_repeaters = '/home/miguelm/Documents/Anisotropies/Repeater_Analysis/DataSets/MockData_Repeaters/'
+# PERIOD_OF_REP = '3600'
+# N_ACCEPTED_REP_EVENTS = '100'
+# N_INTENSITY = '5'
+#
+# df_repeater = pd.read_parquet(path_to_repeaters + 'Rep_events_with_tau_Period_' + PERIOD_OF_REP + '_TotalEvents_100000_AcceptedRepEvents_' + N_ACCEPTED_REP_EVENTS + '_MaxRepIntensity_' + N_INTENSITY + '.parquet', engine='fastparquet')
+#
+# #store tau values in sidereal days
+# tau_repeater = np.divide(df_repeater["tau (s)"].to_numpy(),86164)
 
-df_repeater = pd.read_parquet(path_to_repeaters + 'Rep_events_with_tau_Period_' + PERIOD_OF_REP + '_TotalEvents_100000_AcceptedRepEvents_' + N_ACCEPTED_REP_EVENTS + '_MaxRepIntensity_' + N_INTENSITY + '.parquet', engine='fastparquet')
+#----------------------
+# store the tau distribution from auger data and saves important info from the selection info file
+#----------------------
+path_to_auger_data = '../results/'
+auger_data_file = 'AugerOpenData_InclinedEvents_with_tau.parquet'
+auger_selection_file = 'AugerOpenData_InclinedEvents_SelectionInfo.parquet'
+
+auger_data = pd.read_parquet(path_to_auger_data + auger_data_file, engine='fastparquet')
+auger_selection_info = pd.read_parquet(path_to_auger_data + auger_selection_file, engine='fastparquet')
 
 #store tau values in sidereal days
-tau_repeater = np.divide(df_repeater["tau (s)"].to_numpy(),86164)
+tau_auger = np.divide(auger_data["tau (s)"].to_numpy(), 86164)
 
+#stores important info from auger selection info file
+N_events = int(auger_selection_info.iloc[0]['N_events'])
+theta_min = float(auger_selection_info.iloc[0]['Theta_min'])
+theta_max = float(auger_selection_info.iloc[0]['Theta_max'])
+ang_window = math.degrees(float(auger_selection_info.iloc[0]['Ang_window']))
+t_begin = Time(auger_selection_info.iloc[0]['t_begin'], format='fits').gps
+t_end = Time(auger_selection_info.iloc[0]['t_end'], format = 'fits').gps
+
+#computes the average rate of events in events per sidereal day
+average_rate = 86164 * N_events/(t_end - t_begin)
+
+print('Average rate', average_rate)
 #---------------------------------------
 # To plot histograms of tau distribution
 #---------------------------------------
 
 #perform KS test to verify the compatibity between tau dists with and without repeater
-ks_stat_value, ks_p_value = stats.kstest(tau_repeater, tau_ud_all)
+ks_stat_value, ks_p_value = stats.kstest(tau_auger, tau_ud_all)
 
 #plot log10 of tau distributions
 fig_tau_log = plt.figure(figsize=(10,8)) #create figure
 ax_tau_log = fig_tau_log.add_subplot(111) #create subplot with a set of axis with
 
-ax_tau_log.hist(np.log10(tau_repeater),bins=200, range=[-5,4], alpha = 0.5, label=r'Isotropy + ' + N_ACCEPTED_REP_EVENTS + r' events from repeater w/ $\tau = 1$ hour')
+ax_tau_log.hist(np.log10(tau_auger), bins=100, range=[-2,4], alpha = 0.5, label=r'Auger Data: $N_{\textrm{evt}} = {%i}$, ${%.0f}^\circ < \theta < {%.0f}^\circ$' % (N_events, theta_min, theta_max))
 
-log_tau_avg_hist_edges, log_tau_avg_hist_content = AverageTauDist(list_of_log_tau_arrays, 200, -5, 4)
+log_tau_avg_hist_edges, log_tau_avg_hist_content = AverageTauDist(list_of_log_tau_arrays, 100, -2, 4)
 
 ax_tau_log.plot(log_tau_avg_hist_edges, log_tau_avg_hist_content, label=r'Isotropy')
+#ax_tau_log.plot(np.arange(-2,4,0.01), len(tau_auger)*LogExpEnvelop(np.arange(-2,4,0.01), average_rate), color = 'purple', linestyle='--', label=r'Exponential Envelop',)
+
 ax_tau_log.plot([],[],lw=0,label=r'KS test: $p$-value = {%.3f}' % ks_p_value)
 
-ax_tau_log.set_title(r'$\log_{10}(\tau)$ distribution for angular window $\Delta \theta = 1^\circ$', fontsize=24)
+ax_tau_log.set_title(r'$\log_{10}(\tau)$ distribution for angular window $\Delta \theta = {%.0f}^\circ$' % ang_window, fontsize=24)
 ax_tau_log.set_xlabel(r'$\log_{10}(\tau/ \textrm{sidereal days})$', fontsize=20)
 ax_tau_log.set_ylabel(r'Number of pairs', fontsize=20)
 ax_tau_log.tick_params(axis='both', which='major', labelsize=20)
 ax_tau_log.set_yscale('log')
-ax_tau_log.set_ylim(1e-2, 1e4)
+ax_tau_log.set_ylim(1e-2, 1e2)
 
 ax_tau_log.legend(loc='best', fontsize=18)
 
-fig_tau_log.savefig('./results/Average_log10tau_distribution_wRepeater_RandPos_' + PERIOD_OF_REP + '.pdf')
+fig_tau_log.savefig('./AugerInclinedData/Average_log10tau_distribution_AugerInclinedData.pdf')
 
 #--------------------------------------
 # plot of tau distributions
 #--------------------------------------
-fig_tau = plt.figure(figsize=(10,8)) #create figure
-ax_tau = fig_tau.add_subplot(111) #create subplot with a set of axis with
-
-ax_tau.hist(tau_repeater,bins=200, range=[0,10], alpha = 0.5, label=r'Isotropy + ' + N_ACCEPTED_REP_EVENTS + r' events from repeater w/ $\tau = 1$ hour')
-
-tau_avg_hist_edges, tau_avg_hist_content = AverageTauDist(list_of_ordered_taus_ud, 200, 0, 10)
-
-ax_tau.plot(tau_avg_hist_edges, tau_avg_hist_content, label=r'Isotropy')
-
-ax_tau.set_title(r'$\tau$ distribution for angular window $\Delta \theta = 1^\circ$',fontsize=24)
-ax_tau.set_xlabel(r'$\tau$ (sidereal days)', fontsize=20)
-ax_tau.set_ylabel(r'Number of pairs', fontsize=20)
-ax_tau.tick_params(axis='both', which='major', labelsize=20)
-ax_tau.set_xlim([0,10])
-ax_tau.set_yscale('log')
-ax_tau.set_ylim(1e-1,5e2)
-
-ax_tau.legend(loc='upper right', fontsize=18)
-
-fig_tau.savefig('./results/Average_tau_distribution_wRepeater_RandPos_' + PERIOD_OF_REP + '.pdf')
-
+# fig_tau = plt.figure(figsize=(10,8)) #create figure
+# ax_tau = fig_tau.add_subplot(111) #create subplot with a set of axis with
+#
+# ax_tau.hist(tau_auger,bins=200, range=[0,10], alpha = 0.5, label=r'Isotropy + ' + N_ACCEPTED_REP_EVENTS + r' events from repeater w/ $\tau = 1$ hour')
+#
+# tau_avg_hist_edges, tau_avg_hist_content = AverageTauDist(list_of_ordered_taus_ud, 200, 0, 10)
+#
+# ax_tau.plot(tau_avg_hist_edges, tau_avg_hist_content, label=r'Isotropy')
+#
+# ax_tau.set_title(r'$\tau$ distribution for angular window $\Delta \theta = 1^\circ$',fontsize=24)
+# ax_tau.set_xlabel(r'$\tau$ (sidereal days)', fontsize=20)
+# ax_tau.set_ylabel(r'Number of pairs', fontsize=20)
+# ax_tau.tick_params(axis='both', which='major', labelsize=20)
+# ax_tau.set_xlim([0,10])
+# ax_tau.set_yscale('log')
+# ax_tau.set_ylim(1e-1,5e2)
+#
+# ax_tau.legend(loc='upper right', fontsize=18)
+#
+# fig_tau.savefig('./results/Average_tau_distribution_wRepeater_RandPos_' + PERIOD_OF_REP + '.pdf')
+#
 #---------------------------------------
 # plot of cdf of log tau
 #---------------------------------------
 fig_cdf_tau_log = plt.figure(figsize=(10,8)) #create figure
 ax_cdf_tau_log = fig_cdf_tau_log.add_subplot(111) #create subplot with a set of axis with
 
-cdf_rep_bin_edges, cdf_rep_content = ComulativeDistFunc(np.log10(tau_repeater), 200)
-cdf_ud_bin_edges, cdf_ud_content = ComulativeDistFunc(np.log10(tau_ud_all), 200)
+cdf_rep_bin_edges, cdf_rep_content = ComulativeDistFunc(np.log10(tau_auger), 100)
+cdf_ud_bin_edges, cdf_ud_content = ComulativeDistFunc(np.log10(tau_ud_all), 100)
 
+ax_cdf_tau_log.plot(cdf_rep_bin_edges, cdf_rep_content, label=r'Auger Data: $N_{\textrm{evt}} = {%i}$, ${%.0f}^\circ < \theta < {%.0f}^\circ$' % (N_events, theta_min, theta_max))
 ax_cdf_tau_log.plot(cdf_ud_bin_edges, cdf_ud_content, label=r'Isotropy')
-ax_cdf_tau_log.plot(cdf_rep_bin_edges, cdf_rep_content, label=r'Isotropy + ' + N_ACCEPTED_REP_EVENTS + r' events from repeater w/ $\tau = 1$ hour')
 
-ax_cdf_tau_log.set_title(r'$N(\log_{10}(\tau))$ for angular window $\Delta \theta = 1^\circ$', fontsize=24)
+ax_cdf_tau_log.set_title(r'$N(\log_{10}(\tau))$ for angular window $\Delta \theta = {%.0f}^\circ$' % ang_window, fontsize=24)
 ax_cdf_tau_log.set_xlabel(r'$\log_{10}(\tau/ \textrm{sidereal days})$', fontsize=20)
 ax_cdf_tau_log.set_ylabel(r'Arb. Units', fontsize=20)
 ax_cdf_tau_log.tick_params(axis='both', which='major', labelsize=20)
@@ -217,8 +253,7 @@ ax_cdf_tau_log.set_yscale('log')
 
 ax_cdf_tau_log.legend(loc='best', fontsize=18)
 
-fig_cdf_tau_log.savefig('./results/Average_log10tau_CDF_wRepeater_RandPos_' + PERIOD_OF_REP + '.pdf')
-
+fig_cdf_tau_log.savefig('./AugerInclinedData/Average_log10tau_CDF_AugerInclinedData.pdf')
 
 #list with the integration limits given in sidereal days!!!!!
 list_of_integration_lims = [0,1]
@@ -232,16 +267,16 @@ for i in range(1,len(list_of_integration_lims)):
     upper_lim = list_of_integration_lims[i]
 
     #estimator value for repeater mock data set
-    repeater_estimator = len( [tau for tau in tau_repeater if (tau > lower_lim and tau < upper_lim)])
+    auger_data_estimator = len( [tau for tau in tau_auger if (tau > lower_lim and tau < upper_lim)])
 
     estimator_list = EstimatorDist(list_of_ordered_taus_ud, lower_lim, upper_lim)
     #estimator_list_rep = EstimatorDist(list_of_ordered_taus_rep, lower_lim, upper_lim)
 
     #compute the number of RMS between average and mockdata set point
-    print('Estimator for Mock data set is', abs(np.mean(estimator_list) - repeater_estimator)/math.sqrt(np.var(estimator_list)),'sigma away from estimator dist mean')
+    print('Estimator for Mock data set is', abs(np.mean(estimator_list) - auger_data_estimator)/math.sqrt(np.var(estimator_list)),'sigma away from estimator dist mean')
 
     #integral above the repeater datum point
-    above_rep_data = len([est for est in estimator_list if est >= repeater_estimator])
+    above_rep_data = len([est for est in estimator_list if est >= auger_data_estimator])
     p_value = above_rep_data/len(estimator_list)
 
     print('p-value for tau distribution estimator', p_value)
@@ -255,29 +290,29 @@ for i in range(1,len(list_of_integration_lims)):
     content, bins, _ = ax_est.hist(estimator_list, bins = max(estimator_list) - min(estimator_list), range=[min(estimator_list), max(estimator_list)], alpha=0.5, label='Uniform distribution')
     #content_rep, bins_rep, _ = ax_est.hist(estimator_list_rep, bins = max(estimator_list_rep) - min(estimator_list_rep), range=[min(estimator_list_rep), max(estimator_list_rep)], alpha=0.5, label='Repeater distribution')
 
-    ax_est.axvline(repeater_estimator, 0, max(content), linestyle = 'dashed', color = 'darkorange', label=r'Repeater data')
+    ax_est.axvline(auger_data_estimator, 0, max(content), linestyle = 'dashed', color = 'darkorange', label=r'Auger data')
 
     #fit the distribution of the estimator
     #sigma_data = np.sqrt(content)
 
     #print(sigma_data)
 
-    init_parameters = [np.mean(estimator_list), math.sqrt(np.var(estimator_list)), 1, 0]
-
-    parameters, covariance = curve_fit(Gaussian, np.array(bins[1:]), np.array(content), p0=init_parameters) # sigma = sigma_data, absolute_sigma=True)
-
-    x_gauss_fit = np.arange(min(estimator_list), max(estimator_list), 0.01)
-    y_gauss_fit = Gaussian(x_gauss_fit, *parameters)
-
-    #expected value for each bin
-    expected_bin_content = []
-
-    for i in range(len(bins)-1):
-        expected_bin_content.append(Gaussian((bins[i+1] + bins[i])/2, *parameters))
-
-    #fit_chi_square, p_value_chisrq = chisquare(content, f_exp = expected_bin_content, ddof= int(len(content) - len(parameters))) #Chi_Square(content, expected_bin_content, sigma_data)/()
-
-    ax_est.plot(x_gauss_fit, y_gauss_fit, color='purple', linewidth=2) #, label=r'$\chi^2/$ndf = %.2f' % fit_chi_square)
+    # init_parameters = [np.mean(estimator_list), math.sqrt(np.var(estimator_list)), 1, 0]
+    #
+    # parameters, covariance = curve_fit(Gaussian, np.array(bins[1:]), np.array(content), p0=init_parameters) # sigma = sigma_data, absolute_sigma=True)
+    #
+    # x_gauss_fit = np.arange(min(estimator_list), max(estimator_list), 0.01)
+    # y_gauss_fit = Gaussian(x_gauss_fit, *parameters)
+    #
+    # #expected value for each bin
+    # expected_bin_content = []
+    #
+    # for i in range(len(bins)-1):
+    #     expected_bin_content.append(Gaussian((bins[i+1] + bins[i])/2, *parameters))
+    #
+    # #fit_chi_square, p_value_chisrq = chisquare(content, f_exp = expected_bin_content, ddof= int(len(content) - len(parameters))) #Chi_Square(content, expected_bin_content, sigma_data)/()
+    #
+    # ax_est.plot(x_gauss_fit, y_gauss_fit, color='purple', linewidth=2) #, label=r'$\chi^2/$ndf = %.2f' % fit_chi_square)
 
     #ax_est.plot([],linewidth=0, label=r'$\hat{I} = \displaystyle\int_{%i}^{%i} \displaystyle\frac{\textrm{d} N}{\textrm{d} \tau} \textrm{d}\tau$' % (lower_lim,upper_lim))
     ax_est.plot([],linewidth=0, label=r'$p$-value = {%.3f}' % (p_value))
@@ -286,8 +321,8 @@ for i in range(1,len(list_of_integration_lims)):
     ax_est.set_xlabel(r'$\hat{N}(%.0f < \tau < %.0f$ days)' % (lower_lim, upper_lim), fontsize=20)
     ax_est.set_ylabel(r'Arb. units', fontsize=20)
     ax_est.tick_params(axis='both', which='major', labelsize=20)
-    ax_est.legend(loc='upper left', fontsize=18)
+    ax_est.legend(loc='upper right', fontsize=18)
 
-    fig_est.savefig('./results/Estimator_distribution_histogram_RandPos_' + PERIOD_OF_REP + '.pdf')
+    fig_est.savefig('./AugerInclinedData/Estimator_distribution_histogram_AugerInclinedData.pdf')
 
     print('A 5 sigma excess in our estimator corresponds to', 5*math.sqrt(np.var(estimator_list)))
--- Tau_Distribution/Compute_Tau_Dist.py
+++ Tau_Distribution/Compute_Tau_Dist.py
@@ -26,8 +26,35 @@ from Class_Event import Event
 rc('font',**{'family':'serif','serif':['DejaVu Sans']})
 rc('text', usetex=True)
 
-#def AcceptAugerEvent(array):
+#Applies cuts to the auger data set and returns a list with the selected events
+def SelectAugerData(filename, energy_th, theta_min, theta_max, t_min, t_max):
 
+    #saves raw auger data from input file
+    auger_data_raw = pd.read_parquet(filename, engine='fastparquet')
+
+    #applies cuts in energy
+    auger_data = auger_data_raw.loc[(auger_data_raw['sd_theta'] > theta_min) & (auger_data_raw['sd_theta'] < theta_max) & (auger_data_raw['sd_energy'] > energy_th) & (auger_data_raw['sd_gpstime'] > t_min) & (auger_data_raw['sd_gpstime'] < t_max) ]
+
+    #saves the number of events after cuts
+    N_events = len(auger_data.index)
+
+    #saves the gpstime
+    evt_gpstime = auger_data['sd_gpstime'].to_numpy()
+
+    #list to hold events
+    event_list = []
+
+    for i in range(N_events):
+        time = auger_data.iloc[i]['sd_gpstime']
+        ra = math.radians(auger_data.iloc[i]['sd_ra'])
+        dec = math.radians(auger_data.iloc[i]['sd_dec'])
+        energy = auger_data.iloc[i]['sd_energy']
+
+        event_list.append(Event(ra, dec, time, energy))
+
+    return min(evt_gpstime), max(evt_gpstime), event_list
+
+#checks if event is in the angular window centered at another event
 def EventInSphericalCap(alpha_center, delta_center, alpha, delta, ang_window):
 
     ang_sep = math.acos(math.cos(delta_center)*math.cos(delta)*math.cos(alpha_center - alpha) + math.sin(delta_center)*math.sin(delta))
@@ -37,57 +64,66 @@ def EventInSphericalCap(alpha_center, delta_center, alpha, delta, ang_window):
     else:
         return True
 
-#name of file with repeater
-if len(sys.argv) == 1:
-    raise Exception('Please insert the name of the event file!')
-
-filename = './output/' + sys.argv[1]
-
-# date = '2015-01-01T00:00:00'
-# period = '3600.0'
-# N_total_events = '100'
-# N_accepted_rep_events = '68'
+#name of file with
+# if len(sys.argv) == 1:
+#     raise Exception('Please insert the name of the event file!')
 #
+# filename = './output/' + sys.argv[1]
+#
+# # date = '2015-01-01T00:00:00'
+# # period = '3600.0'
+# # N_total_events = '100'
+# # N_accepted_rep_events = '68'
+# #
 # #verifies the existence of file
 # filename = './output/TimeOrdered_Events_ExponentialRepeater_Date_' + date + '_Period_' + period + '_TotalEvents_' + N_total_events + '_AcceptedRepEvents_' + N_accepted_rep_events + '.parquet'
 
-try:
-    file = open(filename)
-except FileNotFoundError:
-    print('File not found')
-finally:
-    file.close()
+# try:
+#     file = open(filename)
+# except FileNotFoundError:
+#     print('File not found')
+# finally:
+#     file.close()
 
-#read file with shower data
-data_auger = pd.read_csv('./input/dataSummary.csv')
-data_rep = pd.read_parquet(filename, engine='fastparquet')
+#defines the cuts to apply to auger data
+energy_th = math.sqrt(10)
+theta_min = 60
+theta_max = 80
 
-print(data_rep)
+#saves auger events after appyling the cuts
+path_to_data = '../DataSets/Auger_Pub_data/'
+data_file = 'AugerOpenData_InclinedEvents_eFit.parquet'
+t_min, t_max, accepted_events_list = SelectAugerData(path_to_data + data_file, energy_th, theta_min, theta_max, Time('2004-01-01T00:00:00', format='fits').gps, Time('2021-12-31T23:59:59', format='fits').gps)
+
+#print the number of events and the dates between which events are being considered
+print('Total number of events:', len(accepted_events_list), 'between', Time(t_min, format='gps').fits, 'and', Time(t_max, format='gps').fits)
+
+#print(data_rep)
 
 #convert relevant pandas columns into np arrays
-auger_gpstime = data_auger['gpstime'].to_numpy()
-
-rep_event_timestamp = data_rep["rep_ud_gpstime"].to_numpy()
-rep_event_energy = data_rep["rep_ud_energy"].to_numpy()
-rep_event_alpha = data_rep["rep_ud_ra"].to_numpy()
-rep_event_delta = data_rep["rep_ud_dec"].to_numpy()
-
-#define the minimum date, energy and zenith angle
-time_min = Time(str(min(auger_gpstime)), scale='utc', format='gps').gps
-time_max = Time(str(max(auger_gpstime)), scale='utc', format='gps').gps
-
-#create list of accepted events
-accepted_rep_events_list = []
-
-#list to hold events with repeater and uniform background
-for i in range(len(rep_event_timestamp)):
-    #time = Time(str(rep_event_timestamp[i]),format='gps')
-    accepted_rep_events_list.append(Event(rep_event_alpha[i], rep_event_delta[i],rep_event_timestamp[i], rep_event_energy[i]))
-
-#saves number of accepted events
-N_accepted_rep_events = len(accepted_rep_events_list)
-
-print('Number of events with repeater and uniform background:', N_accepted_rep_events)
+# auger_gpstime = data_auger['gpstime'].to_numpy()
+#
+# rep_event_timestamp = data_rep["rep_ud_gpstime"].to_numpy()
+# rep_event_energy = data_rep["rep_ud_energy"].to_numpy()
+# rep_event_alpha = data_rep["rep_ud_ra"].to_numpy()
+# rep_event_delta = data_rep["rep_ud_dec"].to_numpy()
+#
+# #define the minimum date, energy and zenith angle
+# time_min = Time(str(min(auger_gpstime)), scale='utc', format='gps').gps
+# time_max = Time(str(max(auger_gpstime)), scale='utc', format='gps').gps
+#
+# #create list of accepted events
+# accepted_rep_events_list = []
+#
+# #list to hold events with repeater and uniform background
+# for i in range(len(rep_event_timestamp)):
+#     #time = Time(str(rep_event_timestamp[i]),format='gps')
+#     accepted_rep_events_list.append(Event(rep_event_alpha[i], rep_event_delta[i],rep_event_timestamp[i], rep_event_energy[i]))
+#
+# #saves number of accepted events
+# N_accepted_rep_events = len(accepted_rep_events_list)
+#
+# print('Number of events with repeater and uniform background:', N_accepted_rep_events)
 
 #angular window
 ang_window = np.radians(1)
@@ -99,13 +135,13 @@ task1_begin = datetime.now()
 tau_rep = []
 
 #vector to save events with respective tau
-accepted_rep_events_with_tau = []
+accepted_events_with_tau = []
 event_counter = 0
 
 #compute the time difference between two consecutive events for repeater with uniform background
-for evt1 in accepted_rep_events_list:
+for evt1 in accepted_events_list:
 
-    for evt2 in accepted_rep_events_list:
+    for evt2 in accepted_events_list:
 
         if evt2 > evt1:
 
@@ -115,38 +151,32 @@ for evt1 in accepted_rep_events_list:
             if EventInSphericalCap(evt1.alpha, evt1.delta, evt2.alpha, evt2.delta, ang_window):
                 tau = evt2.time - evt1.time
                 tau_rep.append(tau)
-                accepted_rep_events_with_tau.append([evt1.alpha, evt1.delta, evt1.time, evt1.energy, evt2.alpha, evt2.delta, evt2.time, evt2.energy, tau])
+                accepted_events_with_tau.append([evt1.alpha, evt1.delta, evt1.time, evt1.energy, evt2.alpha, evt2.delta, evt2.time, evt2.energy, tau])
                 break
 
     event_counter+=1
 
     print(event_counter,'events done!')
 
-print('The computation for tau for', N_accepted_rep_events, ' events with repeater and uniform background took', datetime.now() - task1_begin)
+
+print('The computation for tau for', len(accepted_events_list), ' events took', datetime.now() - task1_begin)
 
 #print file with taus
 #output_ud_data = pd.DataFrame(tau_ud,columns=['tau (s)'])
 #output_rep_data = pd.DataFrame(tau_rep,columns=['tau (s)'])
 
 #print file with events and respective taus
-output_rep_data = pd.DataFrame(accepted_rep_events_with_tau,columns=['evt1_ra (rad)','evt1_dec (rad)','evt1_gpstime','evt1_energy (EeV)', 'evt2_ra (rad)','evt2_dec (rad)','evt2_gpstime','evt2_energy (EeV)','tau (s)'])
+output_data = pd.DataFrame(accepted_events_with_tau, columns=['evt1_ra (rad)','evt1_dec (rad)','evt1_gpstime','evt1_energy (EeV)', 'evt2_ra (rad)','evt2_dec (rad)','evt2_gpstime','evt2_energy (EeV)','tau (s)'])
 
-print(output_rep_data)
+print(output_data)
 
-#save csv file with data
-output_rep_data.to_parquet('./output/Rep_events_with_tau_Date_' + date + '_Period_' + period + '_N_' + N_events + '.parquet', index=False)
-
-#define energy and theta threshold
-theta_max = 60
-
-print(type(ang_window))
+#save data in file
+output_data.to_parquet('./results/AugerOpenData_InclinedEvents_with_tau.parquet', index=False)
 
 #export output file with relevant parameters
-selection_info_ud = np.array([N_accepted_rep_events, math.nan, theta_max, ang_window, Time(str(time_min),format='gps').fits, Time(str(time_max),format='gps').fits])
+selection_info = np.array([len(accepted_events_list), energy_th, theta_min, theta_max, ang_window, Time(t_min,format='gps').fits, Time(t_max,format='gps').fits])
 
-print([selection_info_ud])
-
-output_info = pd.DataFrame([selection_info_ud], columns=["N_events","E_th","Theta_max","Ang_window","t_begin","t_end"])
-output_info.to_parquet('./output/Selection_event_info_Date_' + date + '_Period_' + period + '_N_' + N_events + '.parquet', index=False)
+output_info = pd.DataFrame([selection_info], columns=["N_events","E_th","Theta_min", "Theta_max","Ang_window","t_begin","t_end"])
+output_info.to_parquet('./results/AugerOpenData_InclinedEvents_SelectionInfo.parquet', index=False)
 
 print(output_info.info())
