6708ae0c6fec17e1fafcc71edf9ad8ea8057f235
miguelajsmartins <miguelalexandre.jesusdasilva@usc.es>
2022-05-24 18:24:45 +0200

    Modified copy_files.sh

--- DataSets/copy_files.sh
+++ DataSets/copy_files.sh
@@ -2,8 +2,8 @@
 
 OUTPUTDIR="Vertical/MockData_Repeaters/Repeater_RandPosAndDate_Catalog_AugerOpenData_stats"
 HOSTNAME="miguel.martins@nodo014"
-PATH2FILE="/data9/mmartins/repeater_analysis_output/"
-FILE2COPY="OUTPUT_3843050_*/REP_VerticalEvents_with_tau_RandPosAndDate_Period_3600_TotalEvents_13068_AcceptedRepEvents_12_RepIntensity_2_3843050_**.parquet"
+PATH2FILE="/data9/mmartins/repeater_analysis_output/Vertical/Repeater_RandPosAndDate_Catalog_AugerOpenData_stats"
+FILE2COPY="OUTPUT_*/REP_VerticalEvents_with_tau_**.parquet"
 
 read -p "Are you sure you want to copy $FILE2COPY (y/n): " answser
 
