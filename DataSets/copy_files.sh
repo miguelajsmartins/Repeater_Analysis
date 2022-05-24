2696685e9bacdf3f1a0cf9872b952034fe6fb3fb
Miguel Martins <miguelalexandre.jesusdasilva@usc.es>
2022-05-24 17:52:55 +0200

    Added the code to compute the Li and Ma significance

--- DataSets/copy_files.sh
+++ DataSets/copy_files.sh
@@ -3,8 +3,7 @@
 OUTPUTDIR="Vertical/MockData_Repeaters/Repeater_RandPosAndDate_large_stats"
 HOSTNAME="miguel.martins@nodo014"
 PATH2FILE="/data9/mmartins/repeater_analysis_output/Vertical/Repeater_RandPosAndDate_large_stats"
-FILE2COPY="OUTPUT_3843630_*/REP_VerticalEvents_with_tau_RandPosAndDate_Period_3600_TotalEvents_100000_AcceptedRepEvents_200_RepIntensity_5_3843630_**.parquet"
-
+FILE2COPY="OUTPUT_3843630_*/IsoBG_ExpRepeater_RandPosAndDate_Period_3600_TotalEvents_100000_AcceptedRepEvents_200_RepIntensity_5_3843630_**.parquet"
 read -p "Are you sure you want to copy $FILE2COPY (y/n): " answser
 
 if [ $answser == "y" ]
